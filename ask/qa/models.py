9dbbaf83ad62e5d91b3e5107c3a4b4fc58e1a801
artemgl <artem_chernikov00@list.ru>
2022-05-31 21:43:37 +0000

    Added database

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,27 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    
+    def popular(self):
+        return self.order_by('-rating')
+
+class Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    rating = models.IntegerField()
+    added_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, related_name="question_author")
+    likes = models.ManyToManyField(User)
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User, related_name="answer_author")
--- init.sh
+++ init.sh
@@ -4,4 +4,10 @@ sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/gunicorn_wsgi.conf /etc/gunicorn.d/
 
+sudo /etc/init.d/mysql restart
+sudo mysql -uroot -e "create database if not exists ask_db"
+
+sudo python /home/box/web/ask/manage.py makemigrations
+sudo python /home/box/web/ask/manage.py migrate
+
 sudo gunicorn --chdir /home/box/web/ask/ -b 0.0.0.0:8000 ask.wsgi:application
