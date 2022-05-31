ebe671adf3f453fe2f5d7673dd3aa77274daa201
artemgl <43479457+artemgl@users.noreply.github.com>
2022-06-01 01:39:10 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,6 +8,6 @@ sudo /etc/init.d/mysql restart
 sudo mysql -uroot -e "create database if not exists ask_db"
 
 sudo python /home/box/web/ask/manage.py makemigrations qa
-sudo python /home/box/web/ask/manage.py migrate qa
+sudo python /home/box/web/ask/manage.py migrate
 
 sudo gunicorn --chdir /home/box/web/ask/ -b 0.0.0.0:8000 ask.wsgi:application
