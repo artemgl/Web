fc9f64887bd093f1a0732d5d61cbb2d2f1774cde
artemgl <artem_chernikov00@list.ru>
2022-05-31 10:23:17 +0000

    Changed gunicorn script

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/gunicorn_wsgi.conf /etc/gunicorn.d/
 
-sudo gunicorn -w 2 -b 0.0.0.0:8080 hello:wsgi_application & gunicorn -w 2 -b 0.0.0.0:8000 /home/box/web/etc/qa.py ask.wsgi:application
+gunicorn -w 2 -b 0.0.0.0:8080 hello:wsgi_application & gunicorn -w 2 -b 0.0.0.0:8000 ask.wsgi:application
